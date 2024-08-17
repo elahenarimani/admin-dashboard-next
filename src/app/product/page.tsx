3f858ba24471fef642d892c75c57c632529ff076
Elahe Narimani <elahe.narimani@gmail.com>
2024-08-17 21:30:25 +0330

    add first commit

---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+// function ProductPage({children,}: Readonly<{
+//     children: React.ReactNode;
+//   }>){
+function ProductPage(){
+return(
+  <div className="w-full h-full">
+  <p>jkhhjgjhgj</p>
+  <p>hi</p>
+      </div>
+)
+}
+export default ProductPage;
