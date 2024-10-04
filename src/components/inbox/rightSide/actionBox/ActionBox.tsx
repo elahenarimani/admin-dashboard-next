3bcdbbd45df644ca49a7b3b982d3bc1562fd1c78
Elahe Narimani <elahe.narimani@gmail.com>
2024-10-04 22:09:19 +0330

    fix action box

--- src/components/inbox/rightSide/actionBox/ActionBox.tsx
+++ src/components/inbox/rightSide/actionBox/ActionBox.tsx
@@ -1,35 +1,51 @@
-import React from 'react'
+import React from "react";
 import { FaTrash } from "react-icons/fa6";
 import { IoMdArchive } from "react-icons/io";
 import { BiSolidEnvelopeOpen } from "react-icons/bi";
+import Button from "../../../button/Button";
 interface IInbox {
-    id: number;
-    from: string;
-    subject: string;
-    message: string;
-    timestamp: string;
-    isRead: boolean;
-    label: string;
-  }
-interface IActionBox{
-    inbox :IInbox[], 
-    setInbox : Function,
-    selectedEmails : number[],
-    setSelectedEmails : Function,
+  id: number;
+  from: string;
+  subject: string;
+  message: string;
+  timestamp: string;
+  isRead: boolean;
+  label: string;
 }
-const ActionBox = ({inbox, setInbox , selectedEmails , setSelectedEmails }:IActionBox) => {
-    const handleDelete = () =>{
-        const newInbox = inbox.filter(email => (!selectedEmails.includes(email.id)))// Create a new array with unselected inbox
-        setInbox(newInbox);//selected emails are removed
-        setSelectedEmails([]);
-       
-    }
+interface IActionBox {
+  inbox: IInbox[];
+  setInbox: Function;
+  selectedEmails: number[];
+  setSelectedEmails: Function;
+}
+const ActionBox = ({
+  inbox,
+  setInbox,
+  selectedEmails,
+  setSelectedEmails,
+}: IActionBox) => {
+  const handleDelete = () => {
+    const newInbox = inbox.filter(
+      (email) => !selectedEmails.includes(email.id)
+    ); // Create a new array with unselected inbox
+    setInbox(newInbox); //selected emails are removed
+    setSelectedEmails([]);
+  };
   return (
-    <div className='w-[130px] h-[40px] flex justify-start items-center border-[2px] border-solid border-[#EFEFEF] rounded-[12px] bg-[#F5F6FA]'>
-      <button className='w-full h-full flex justify-center items-center'><IoMdArchive className='w-[16px] h-[16px]'/></button>
-      <button className='w-full h-full border-r-[2px] border-solid border-[#EFEFEF] flex justify-center items-center'  onClick={handleDelete}><FaTrash  className='w-[16px] h-[16px]'/></button>
-      <button className=' w-full h-full border-r-[2px] border-solid border-[#EFEFEF] flex justify-center items-center' ><BiSolidEnvelopeOpen  className='w-[16px] h-[16px]'/></button>
+    <div className="w-[130px] h-[40px] grid grid-cols-3  border-[2px] border-solid border-[#EFEFEF] rounded-[12px] bg-[#F5F6FA]">
+      <Button className="w-full  h-full flex justify-center items-center">
+        <IoMdArchive className="w-[16px] h-[16px]" />
+      </Button>
+      <Button
+        className="w-full h-full border-r-[2px] border-solid border-[#EFEFEF] flex justify-center items-center"
+        onClickHandler={()=>handleDelete()}
+      >
+        <FaTrash className="w-[16px] h-[16px]" />
+      </Button>
+      <Button className=" w-full h-full border-r-[2px] border-solid border-[#EFEFEF] flex justify-center items-center">
+        <BiSolidEnvelopeOpen className="w-[16px] h-[16px]" />
+      </Button>
     </div>
-  )
-}
-export default ActionBox
+  );
+};
+export default ActionBox;
---
+++ src/components/input/Input.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+interface IInput {
+    value: string | number;
+    inputHandler: Function;
+    className: string;
+    placeholder: string;
+    type: string;
+    onClickHandler?:  Function |undefined
+  }
+const Input = ({
+    value,
+    inputHandler,
+    className,
+    placeholder,
+    type,
+    onClickHandler
+  }: IInput) => {
+  return (
+    <div>
+      <input  
+      value={value}
+      onChange={(e:any) => inputHandler(e)}
+      onClick={() => onClickHandler()}
+      type='type'
+      placeholder='placeholder'
+      className='className'
+      ></input>
+    </div>
+  )
+}
+
+export default Input
