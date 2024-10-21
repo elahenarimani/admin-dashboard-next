b8591ec95523e23112556801d8649bf2002c1aff
Elahe Narimani <elahe.narimani@gmail.com>
2024-10-21 21:59:50 +0330

    fix mobile size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,49 @@
-
-
 import ProductPage from "./product/layout";
-import RenderLineChart from "@/components/mainLayOut/renderLineChart/renderLineChart"
+import RenderLineChart from "@/components/mainLayOut/renderLineChart/renderLineChart";
 // import Salesdetail from "@/components/salesDetail/Sales
-
 
 export default function Home() {
   return (
-    <div >
-      <p className="text-[32px] mt-[30px] mb-[23px]">داشبورد</p>
-      <div className="w-full flex justify-between items-center gap-[30px]">
-        <div className="total-users w-[300px] h-[170px] bg-white rounded-[5px]">hi</div>
-        <div  className="total-order w-[300px] h-[170px] bg-white rounded-[5px]">
-         hi
+    <div  className="w-full  ">
+      <p className="w-full text-[32px] mt-[30px] mb-[23px]">داشبورد</p>
+      <div>
+      <div className="w-full grid sm:grid-cols-1 gap-y-8 md:grid-cols-2 2xl:flex 2xl:justify-between items-center justify-items-center pl-0 pr-0 pl-[32px] pr-[32px]">
+        <div className="total-users w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
         </div>
-        <div className="total-sales w-[300px] h-[170px] bg-white rounded-[5px]">hi</div>
-        <div className="total-pending w-[300px] h-[170px] bg-white rounded-[5px]">hi</div>
+        <div className="total-order w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+        <div className="total-sales w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+        <div className="total-pending w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
       </div>
+      </div>
+      {/* <div className="w-full flex justify-between items-center gap-[30px]">
+        <div className="total-users w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+        <div className="total-order w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+        <div className="total-sales w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+        <div className="total-pending w-[300px] h-[170px] bg-white rounded-[5px]">
+          hi
+        </div>
+      </div> */}
       <div className="sales-detail !w-full h-[344px] bg-white rounded-[5px] mt-[30px] pb-[58px] pt-[7px] pl-[32px] pr-[32px] ">
         {/* < Salesdetail/> */}
-          <p className="mt-[50px]">جزئیات فروش</p>
+        <p className="mt-[50px]">جزئیات فروش</p>
         <RenderLineChart />
       </div>
-      <div className="deals-details w-full h-[344px] bg-white rounded-[5px] mt-[30px] pl-[15px] pr-[15px] pt-[20px]">جزئیات معاملات</div>
+      <div className="deals-details w-full h-[344px] bg-white rounded-[5px] mt-[30px] pl-[15px] pr-[15px] pt-[20px]">
+        جزئیات معاملات
+      </div>
     </div>
   );
 }
--- src/components/mainLayOut/sideBar/SideBar.tsx
+++ src/components/mainLayOut/sideBar/SideBar.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 // import { RxDashboard } from "react-icons/rx";
 // import "./sideBar.css"
 import { AiFillMessage } from "react-icons/ai";
 // import DashboardRoundedIcon from '@mui/icons-material/DashboardRoundedIcon';
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
-import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import Link from "next/link";
 const SideBar = () => {
   return (
-    <div className='w-full h-full flex flex-col justify-start items-center !space-y-[18px] pt-[23px] '>
-      <div className='!w-[24px] !min-[24px] !max-[24px] !h-[24px]'>
-        <GridViewRoundedIcon className='w-full h-full opacity-[50%] '/>
-       
-           {/* <RxDashboard className='dashboard !w-full !h-full opacity-[50%]'/> */}
-        
-      </div>
-      <div className='!w-[24px] !h-[24px] '>
-      {/* <RxDashboard className='!w-full !h-full opacity-[50%]'/> */}
-      {/* <svg data-testid="DashboardRoundedIcon"></svg> */}
-           <AiFillMessage className='w-full h-full opacity-[50%] '/>
-        
-      </div>
-    </div>
-  )
-}
+    <ul className="w-full h-full flex flex-col justify-start items-center !space-y-[18px] pt-[23px] ">
+      <li className="!w-[24px] !min-[24px] !max-[24px] !h-[24px]">
+        <Link href="/src/app">
+          <GridViewRoundedIcon className="w-full h-full opacity-[50%] " />
+        </Link>
+      </li>
+      <li className="!w-[24px] !h-[24px] ">
+        <Link href="inbox">
+          <AiFillMessage className="w-full h-full opacity-[50%] " />
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
-export default SideBar
+export default SideBar;
