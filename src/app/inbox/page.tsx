73efa80671285684a953b455580adaa18d9de483
Elahe Narimani <elahe.narimani@gmail.com>
2025-03-25 23:27:11 +0330

    edit code

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,14 +1,6 @@
-import RightSide from '../../components/mainInbox/rightSide/rightSide'
-// import RightSideMob from "@/components/mainInbox/rightSideMob/RightSideMob";
-
+import RightSide from '../../components/mainInbox/rightSide/rightSide';
 import React from "react";
-interface IcompEmail {
-  to: string;
-  subject: string;
-  content: string;
-  id: number;
-}
-export default function InboxPage(){
+export default function InboxPage() {
   return (
     <div className="w-full min-h-screen  bg-[#F5F6FA]">
       <RightSide/>
