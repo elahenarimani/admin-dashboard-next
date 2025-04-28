1218a7b6cf67242780733477e0aafacc135f9d9a
Elahe Narimani <elahe.narimani@gmail.com>
2025-04-28 14:13:40 +0330

    add fetch in product page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 function Custom404() {
   return (
     <div className="w-full min-h-[100vh] flex flex-col justify-start items-center">
-      <h2 className=" text-[37px]">page not found</h2>
+      <h2 className="text-[37px]">page not found</h2>
       <p>could not found requested resource</p>
     </div>
   );
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-
-const ProductPage = () => {
+export default async function ProductPage (){
+  const response = await fetch("https://67e5437718194932a5854cab.mockapi.io/product")
+  const result = await response.json()
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full grid grid-cols-3">
+      {result.map((product:any)=> (
+        <div className="w-full h-full " key={product.id}> <div>{product.name}</div></div>
+      ))}
       
+      <p>yes</p>
     </div>
   );
 };
 
-export default ProductPage;
+
