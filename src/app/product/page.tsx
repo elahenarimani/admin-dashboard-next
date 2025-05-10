297c07890dc5c03193e49492cd3508718f17d5dc
Elahe Narimani <elahe.narimani@gmail.com>
2025-05-10 11:15:09 +0330

    edit code

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-export default async function ProductPage (){
-  const response = await fetch("https://67e5437718194932a5854cab.mockapi.io/product")
-  const result = await response.json()
+export default async function ProductPage() {
+  const response = await fetch(
+    "https://67e5437718194932a5854cab.mockapi.io/product"
+  );
+  const result = await response.json();
   return (
     <div className="w-full h-full grid grid-cols-3">
-      {result.map((product:any)=> (
-        <div className="w-full h-full " key={product.id}> <div>{product.name}</div></div>
+      {result.map((product: any) => (
+        <div className="w-full h-full " key={product.id}>
+          {" "}
+          <div>{product?.name}</div>
+        </div>
       ))}
-      
       <p>yes</p>
     </div>
   );
-};
-
-
+}
