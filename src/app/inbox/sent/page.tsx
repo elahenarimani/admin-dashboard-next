f70fdc99e71e70e7c9cb3f331b8fc6ea55baad1d
Elahe Narimani <elahe.narimani@gmail.com>
2025-05-14 13:33:01 +0330

    edit code

--- src/app/inbox/sent/page.tsx
+++ src/app/inbox/sent/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 import React from "react";
 import RightSideSent from "@/components/mainInbox/rightSideSent/RightSideSent";
-interface ISentPage {
-  inpvalTo: string;
-  inpvalSubject: string;
-  inpvalContent: string;
-  id: number;
-  params?: { sent: string };
-}
-export default function SentPage({
-  inpvalTo,
-  inpvalSubject,
-  inpvalContent,
-  id,
-  params,
-}: ISentPage) {
+export default function SentPage() {
   return (
     <div>
       <RightSideSent />
